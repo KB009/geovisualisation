9c72923ddc64ffb006de9ded0ef0e395131ac627
kikinna <zakopcanova.k@gmail.com>
2016-08-05 15:55:02 +0200

    Changed map, new topoJSON with matching id's
    * a new map since the division of the world of the old one didn't
    correspond to country-based map
    * each country contains an id corresponding to codes in countries.sql

--- js/main.js
+++ js/main.js
@@ -14,46 +14,51 @@ $(window).load(function () {
         active = d3.select(null);
 
     var projection = d3.geo.mercator()
-        .scale(250)
-        .translate([width / 2, height / 2]);
+                            .scale(250)
+                            .translate([width / 2, height / 2]);
 
     var path = d3.geo.path().projection(projection);
     
     var drag = d3.behavior.drag()
-    .origin(function() { return {x: rotate[0], y: -rotate[1]}; })
-    .on("drag", function() {
-      rotate[0] = d3.event.x;
-      projection.rotate(rotate);
-      path = d3.geo.path().projection(projection);
-      d3.selectAll("path").attr("d", path);
-    });  
+                            .origin(function() { return {x: rotate[0], y: -rotate[1]}; })
+                            .on("drag", function() {
+
+                                rotate[0] = d3.event.x;
+                                projection.rotate(rotate);
+                                path = d3.geo.path().projection(projection);
+
+                                d3.selectAll("path").attr("d", path);
+
+                            });  
     
     var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        //.on("click", reset)
-        .call(drag);
+                            .attr("width", width)
+                            .attr("height", height)
+                            //.on("click", reset)
+                            .call(drag);
     
     var g = svg.append("g")
-            .style("stroke-width", "1px");
+                            .style("stroke-width", "1px");
     
     events = d3.json("data/Events-few.txt", function(error, events) {
         console.log(events);
         return events;
     });
 
-    d3.json("data/simple_world.json", function(error, uk) {
+    d3.json("data/world-50m-id.json", function(error, uk) {
         if (error) return console.error(error);
 
         g.selectAll("path")
-                .data(topojson.feature(uk, uk.objects.unit).features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("id", function(d) {
-                    return d.id;
-                })
-                .attr("class", "coutry-boundary")
-                .on("click", clicked);
+                        // .data(topojson.feature(uk, uk.objects.unit).features)
+                        .data(topojson.feature(uk, uk.objects.countries).features)
+                        .enter()
+                        .append("path")
+                        .attr("d", path)
+                        .attr("id", function(d) {
+                            return d.id;
+                        })
+                        .attr("class", "coutry-boundary")
+                        .on("click", clicked);
         
         /*g.append("path")
             .datum(topojson.mesh(uk, uk.objects.unit, function(a, b) { return a !== b; }))
@@ -66,18 +71,19 @@ $(window).load(function () {
         active.classed("active", false);
         active = d3.select(this).classed("active", true);
 
-        var bounds = path.bounds(d),
-            dx = bounds[1][0] - bounds[0][0],
-            dy = bounds[1][1] - bounds[0][1],
-            x = (bounds[0][0] + bounds[1][0]) / 2,
-            y = (bounds[0][1] + bounds[1][1]) / 2,
+        // Zoom on a clicked country
+        var bounds = path.bounds(d),                // [[left, top], [right, bottom]]
+            dx = bounds[1][0] - bounds[0][0],       // right - left
+            dy = bounds[1][1] - bounds[0][1],       // bottom - top
+            x = (bounds[0][0] + bounds[1][0]) / 2,  // (left - rigth) / 2
+            y = (bounds[0][1] + bounds[1][1]) / 2,  // (top - bottom) / 2
             scale = .9 / Math.max(dx / width, dy / height),
             translate = [width / 2 - scale * x, height / 2 - scale * y];
 
         g.transition()
-            .duration(750)
-            .style("stroke-width", 1.5 / scale + "px")
-            .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+                    .duration(750)
+                    .style("stroke-width", 1.5 / scale + "px")
+                    .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
         }
 
         function reset() {
@@ -90,6 +96,7 @@ $(window).load(function () {
             .attr("transform", "");
     }
     
+    // Go to default display / the whole world
     function reset() {
         active.classed("active", false);
         active = d3.select(null);
